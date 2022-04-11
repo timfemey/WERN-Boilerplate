7be533ddd5435d96a3d4608534e3c8633336fd85
Obafemi <81351455+timfemey@users.noreply.github.com>
2022-04-12 00:17:10 +0100

    Shebang added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 console.log("Creating WERN Boilerplate ;)");
 const { spawn } = require("child_process");
 
@@ -5,6 +6,7 @@ let process = spawn("git", [
   "clone",
   "https://github.com/timfemey/WERN-Boilerplate.git",
 ]);
+console.log("Loading...");
 let p = "+";
 process.stdout.on("data", () => {
   p += "+";
@@ -13,11 +15,22 @@ process.stdout.on("data", () => {
 
 process.stderr.on("error", () => {
   console.error(
-    "An Error Occured, check if git installed or network connectivity"
+    "An Error Occured, check if git installed or network connectivity and make sure to use Bash"
   );
 });
 
 process.on("close", () => {
+  let processDel = spawn("rm", ["-rf", "package.json", "index.js"]);
+  processDel.stdout.on("data", () => {
+    p += "+";
+    console.log(p);
+  });
+
+  processDel.stderr.on("error", () => {
+    console.error(
+      "An Error Occured, check if git installed or network connectivity"
+    );
+  });
   let process2 = spawn("cd", ["frontend"]);
   process2.stdout.on("data", () => {
     p += "+";
